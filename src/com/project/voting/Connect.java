80c6e03649edbce3657777e2c5b0c3b3484d6225
Robert O'Brien <rgsobrien@gmail.com>
2013-03-12 20:01:59 +0000

    -Added Connect class for database connection plus Oracle Express jar file.

---
+++ src/com/project/voting/Connect.java
@@ -0,0 +1,59 @@
+package com.project.voting;
+
+
+import java.sql.*;
+import java.io.*;
+import java.lang.*;
+import oracle.jdbc.driver.*; //make sure this is in classpath
+
+
+public class Connect {
+  public static void main (String args [])
+	throws SQLException, IOException {
+    try {
+	  Class.forName("oracle.jdbc.driver.OracleDriver");
+      System.out.println("driver loaded");
+    } catch (ClassNotFoundException e) {
+	    System.out.println ("Could not load the driver");
+	}
+    String servername = "ferdia";
+    String portnumber = "1521";
+    String sid = "ORA11GDB";
+    String url = "jdbc:oracle:thin:@" + servername + ":" + portnumber + ":" + sid;
+    String user, pass;
+	user = readEntry("userid  : ");
+	pass = readEntry("password: ");
+    System.out.println(url);
+    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+	System.out.println ("before connection");
+
+    Connection conn = DriverManager.getConnection(url, user, pass);
+	System.out.println ("after connection");
+    Statement stmt = conn.createStatement ();
+
+    ResultSet rset = stmt.executeQuery
+	("select * from administrators");
+    while (rset.next()) {
+	System.out.println(rset.getString(1) + " " +
+	                   rset.getString(2));
+    }
+    stmt.close();
+    conn.close();
+  }
+//readEntry function -- to read input string
+static String readEntry(String prompt) {
+	   try {
+		 StringBuffer buffer = new StringBuffer();
+		 System.out.print(prompt);
+		 System.out.flush();
+		 int c = System.in.read();
+		 while (c != '\n' && c != -1) {
+		   buffer.append((char)c);
+		   c = System.in.read();
+	     }
+	     return buffer.toString().trim();
+      }  catch (IOException e) {
+		 return "";
+	     }
+  }
+}
